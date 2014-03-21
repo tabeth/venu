f84ab2ebe637002b9b3b3c002e1fd5b47bc78155
Tabeth Nkangoh <benink@gmail.com>
2014-03-21 12:16:17 -0400

    valtwilio

--- valtwilio/settings.py
+++ valtwilio/settings.py
@@ -66,13 +66,7 @@ WSGI_APPLICATION = 'valtwilio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
